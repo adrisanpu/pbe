ec3f52a03ee89e7dca80dbb37d44affa66af994c
adri <adrisanpu@gmail.com>
2020-10-21 13:26:34 +0200

    puzzle1 v1.1

---
+++ rc522_pbe.py
@@ -0,0 +1,17 @@
+from pirc522 import RFID
+
+class Rfid:
+    
+    def read_uid(self):        
+        self.wait_for_tag()
+        (error, data) = self.request()
+        if not error:
+            (error, card_uid) = self.anticoll()
+            uidhex = str(hex(card_uid[0]))+","+str(hex(card_uid[1]))+","+str(hex(card_uid[2]))+","+str(hex(card_uid[3]))                
+        return uidhex
+                               
+    if __name__ == "__main__":
+        print("READING...")
+        rf = RFID()
+        uid = read_uid(rf)       
+        print(uid)
