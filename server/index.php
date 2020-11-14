1493d928c6385fdb4937e0343e8c1117f9560c55
adrisanpu <71441992+adrisanpu@users.noreply.github.com>
2020-11-14 23:14:26 +0100

    Add files via upload

--- server/index.php
+++ server/index.php
@@ -1,8 +1,14 @@
 <?php
 //************COMENTARIS***********
+// OBSERVACIONS:
 // - el uid s'ha de passar desde python al url http://localhost/pbe/get.php?timetables?uid=34737834&day=Fri&ho... 
-// - la taula timetables ha d'estar ordenada per dies.
 // - limit ha d'anar despres de order by
+// A SOLUCIONAR:
+// - ordena be els dies pero els que van abans de la data actual haurien de sortir al final
+// - les funcions showinserver i printinserver es poden fer en una sola?
+// - les funcions del final del fitxer es poden serpar a un nou fitxer
+// - nombrar els fitxers amb noms coherents al seu contingut
+// - revisar la nomenclatura de les variables
 //************COMENTARIS***********
 
 	//dades de la db
@@ -16,7 +22,8 @@
 	$db = new db_connection($dbHost, $dbName, $dbUsr, $dbPassword, "utf8");
 	$connection = $db->connect();
 
-	//agafem el url i separem les dades que ens interessen
+	//agafem el url i separem les dades que ens interessen (taula i vector de constraints)
+	//verifiquem que les dades son correctes amb funcions del fitxer constVeryfier.php
 	require_once("constVeryfier.php");
 	$url= $_SERVER["REQUEST_URI"];
 	$aux = explode('?', $url);
@@ -32,11 +39,84 @@
 		echo "Error en la introducció de la query";
 		exit();
 	}
-	//per a cada query diferent es fa una busqueda a la db amb la sentencia sql pertinent que a mes ordena els registres de la manera que s'especifica a l'enunciat
+
+	//segons la taula que volguem mirar creem una query diferent a la bd i mostrem el seu resultat al servidor
 	switch($table){
 		case "timetables":
 			$i_max = 4;
-			$days = dayParser();
+			orderAndPrintTimetable($connection, $i_max, $constr, $table, $contsVeryfier);				
+			break;
+
+		case "tasks":
+			$i_max = 3;
+			$constr_str = $contsVeryfier->constrCreator($constr, $table);
+			$constr_str = $constr_str. " order by date";
+			printInServer($connection, $constr_str, $i_max);
+			break;
+		case "marks":
+			$i_max = 3;
+			$constr_str = $contsVeryfier->constrCreator($constr, $table);
+			$constr_str = $constr_str. " order by subject";
+			printInServer($connection, $constr_str, $i_max);
+			break;		
+	}
+	
+	//tanquem la conexio amb la db
+	mysqli_close($connection);
+
+	//funcio que retorna un vector amb els resultats de la qurey a la bd 
+	function showInServer($connectDB, $consultDB, $fields){
+		$result = mysqli_query($connectDB, $consultDB);
+		$num_rows = mysqli_num_rows($result); 
+		$out = array_fill(0,$num_rows,"");
+		$j = 0;
+		while($row = mysqli_fetch_row($result)){
+			$i = 1;
+				while($i <= $fields){
+					$out[$j] = $out[$j].$row[$i]. ",";
+					$i ++;
+				}
+			$j++;
+		}
+		return $out;
+	}
+
+	//funcio que retorna un vector de dies ordenats a partir de l'actual.
+	function dayParser(){
+		$actualDate = date("w");
+		$days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		$parsedDays = array(count($days));
+		//ordena els dies a partir del dia actual
+		for($i = 0; $i < count($days); $i++){
+			$parsedDays[$i] = $days[($actualDate+$i)%count($days)];
+		}
+		return $parsedDays;
+	}
+
+	//funcio que detecta si en un vector hi ha algun caracter comparador caracteristic de certes cosntraints
+	function compDetector($constr){
+		foreach ($constr as $value) {
+			$aux = explode("=", $value);
+			foreach ($aux as $value2) {
+				$aux2 = explode("[", $value2);
+				if($aux2[1] != NULL)
+			 		return True;
+			}
+		}
+		return False;
+	}
+
+	//funcio que mostra al server
+	function printInServer($connection, $constr_str, $i_max){
+		$out = showInServer($connection, $constr_str, $i_max);
+			foreach ($out as $value) {
+				echo $value. "<br>";
+			}
+	}
+
+	//funcio que realitza les querys per ordre de dia a partir de l'actual i aplica certes constraints
+	function orderAndPrintTimetable($connection, $i_max, $constr, $table, $contsVeryfier){
+		$days = dayParser();
 			if($constr == NULL || compDetector($constr)){
 				for ($i=0; $i < count($days); $i++) { 
 					if($i != 0){
@@ -58,71 +138,7 @@
 			else{
 				$constr_str = $contsVeryfier->constrCreator($constr, $table);
 				printInServer($connection, $constr_str, $i_max);
-			}				
-			break;
-
-		case "tasks":
-			$i_max = 3;
-			$constr_str = $contsVeryfier->constrCreator($constr, $table);
-			//$constr_str = $constr_str. " order by date";
-			printInServer($connection, $constr_str, $i_max);
-			break;
-		case "marks":
-			$i_max = 3;
-			$constr_str = $contsVeryfier->constrCreator($constr, $table);
-			$constr_str = $constr_str. " order by subject";
-			printInServer($connection, $constr_str, $i_max);
-			break;		
-	}
-
-	//funcio que es crida quan es vol mostrar al servidor el resultat final de la busqueda 
-	function showInServer($connectDB, $consultDB, $fields){
-		$result = mysqli_query($connectDB, $consultDB);
-		$num_rows = mysqli_num_rows($result); 
-		$out = array_fill(0,$num_rows,"");
-		$j = 0;
-		while($row = mysqli_fetch_row($result)){
-			$i = 1;
-				while($i <= $fields){
-					$out[$j] = $out[$j].$row[$i]. ",";
-					$i ++;
-				}
-			$j++;
-		}
-		return $out;
-	}
-	//funcio que ordena per dies a partir del dia actual
-	function dayParser(){
-		$actualDate = date("w");
-		$days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-		$parsedDays = array(count($days));
-		//ordena els dies a partir del dia actual
-		for($i = 0; $i < count($days); $i++){
-			$parsedDays[$i] = $days[($actualDate+$i)%count($days)];
-		}
-		return $parsedDays;
-	}
-
-	function compDetector($constr){
-		foreach ($constr as $value) {
-			$aux = explode("=", $value);
-			foreach ($aux as $value2) {
-				$aux2 = explode("[", $value2);
-				if($aux2[1] != NULL)
-			 		return True;
-			}
-		}
-		return False;
-
-	}
-
-	function printInServer($connection, $constr_str, $i_max){
-		$out = showInServer($connection, $constr_str, $i_max);
-			foreach ($out as $value) {
-				echo $value. "<br>";
 			}
 	}
-	//tanquem la conexio amb la db
-	mysqli_close($connection);
 	//posteriorment python llegeix els valors que apareixen al servidor web
 ?>
